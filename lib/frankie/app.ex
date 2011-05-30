6706067420ec5f238fefb06b9eb0a31f09b7b451
José Valim <jose.valim@gmail.com>
2011-05-30 14:00:00 +0200

    More work on Frankie.

--- lib/frankie/app.ex
+++ lib/frankie/app.ex
@@ -1,19 +1,83 @@
 module Frankie::App
-  object Route
-    attr_reader ['verb, 'route, 'method]
-
-    def initialize(verb, route, method)
-      @('verb: verb, 'route: route, 'method: method)
-    end
-  end
-
+  % An instance of the wrapper object is given to ExBridge when
+  % MyApp.run is invoked. The wrapper is initialized with the MyApp object.
+  %
+  % On each request, the handle_http method is invoked which then tries to
+  % find a matching route. If a matching route is found, we call it, otherwise
+  % we eventually call the error route with 404 status code. The wrapper is
+  % responsible to manage the result given and finally dispatch the response
+  % object.
   object Wrapper
     def initialize(app)
       @('app, app)
     end
 
     def handle_http(request, response)
-      
+      route  = find_route(@app.routes, request)
+      result = route.call(@app, request, response)
+
+      if result.__parent_name__ == 'String
+        result = response.body(result)
+      end
+
+      result.dispatch!
+    end
+
+    private
+
+    def find_route([h|t], request)
+      if h.match?(request)
+        h
+      else
+        handle_http(t)
+      end
+    end
+
+    def find_route([], _, _)
+      Frankie::App::ErrorRoute.new(404)
+    end
+  end
+
+  % Main route object that asserts for the verb,
+  % route and dispatches the stored method/callback.
+  object Route
+    attr_reader ['verb, 'route, 'method]
+
+    def initialize(verb, route, method)
+      @('verb: verb, 'route: route, 'method: method)
+    end
+
+    def match?(request)
+      @method == request.request_method && @path == request.path
+    end
+
+    def call(app, request, response)
+      args = case @method.arity
+      match 2
+        [request, response]
+      match 1
+        [response]
+      match 
+        []
+      end
+
+      @method.bind(app.new).apply(args)
+    end
+  end
+
+  % Route that handle errors.
+  % TODO Make this invoke a hook in the app.
+  object ErrorRoute
+    def initialize(status)
+      @('status, status)
+    end
+
+    def match?(_)
+      true
+    end
+
+    def call(app, request, response)
+      response.set(@status, {}, "Error")
     end
   end
 
@@ -22,33 +86,44 @@ module Frankie::App
       base.set_ivar('routes, [])
     end
 
-    def route(verb, route, method)
-      route_object = Frankie::App::Route.new(verb, route, method)
+    % Adds a new route to the route set. The route object
+    % only needs to respond to a `match?` which receives the
+    % request object and to `call` which receives the application
+    % (not instanciated yet), request and response objects.
+    def bare_route(route_object)
       update_ivar('routes, _.push(route_object))
     end
 
+    % Add a new route with a verb, path and the callback.
+    def route(verb, path, method)
+      bare_route Frankie::App::Route.new(verb, path, method)
+    end
+
     def get(route, method)
-      route 'get, route, method
+      route 'GET, route, method
     end
 
     def post(route, method)
-      route 'post, route, method
+      route 'POST, route, method
     end
 
     def put(route, method)
-      route 'put, route, method
+      route 'PUT, route, method
     end
 
     def patch(route, method)
-      route 'patch, route, method
+      route 'PATCH, route, method
     end
 
     def delete(route, method)
-      route 'delete, route, method
+      route 'DELETE, route, method
     end
 
+    % Start MyApp using the given *server* and *options*. Valid options are
+    % 'port (defaults to 3000) and the process 'name (defaults to
+    % Frankie-#{APP_NAME}).
     def run(server, options := {})
-      options = options.set_new 'name, "Frankie_#{self.__name__}".to_atom
+      options = options.set_new 'name, "Frankie-#{self.__name__}".to_atom
       wrapper = Frankie::App::Wrapper.new(self)
       ExBridge.start server, wrapper, options
     end
--- test/frankie/app_test.exs
+++ test/frankie/app_test.exs
@@ -2,4 +2,15 @@ Code.require_file "../test_helper", __FILE__
 
 object AppTest
   proto ExUnit::Case
+
+  def basic_string_test
+    response = request('get, "/version")
+    {_,_,"1.2.3"} = response.body
+  end
+
+  private
+
+  def request(verb, path)
+    HTTPClient.request(verb, "http://127.0.0.1:3000#{path}")
+  end
 end
