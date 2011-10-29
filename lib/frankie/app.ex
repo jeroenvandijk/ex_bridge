7f4847519859dcef62032e8cb510cf76ee5a88f5
Jeroen van Dijk <jeroen@jeevidee.nl>
2011-10-29 14:48:54 +0200

    First take at trying to get the routing a bit faster by using pattern matching instead of route lookup

--- lib/frankie/app.ex
+++ lib/frankie/app.ex
@@ -1,16 +1,27 @@
 module Frankie::App
   module Routing
-    % Adds a new route to the route set. The route object
-    % only needs to respond to a `match?` which receives the
-    % request object and to `call` which receives the application
-    % (not instanciated yet), request and response objects.
-    def route(route_object)
-      update_ivar 'routes, _.push(route_object)
-    end
 
     % Add a new route with a verb, path and the callback.
     def verb_route(verb, path, method)
-      route(#Frankie::Routes::VerbRoute(verb, path, method))
+      % Unfortunately we can pass the method as a reference to the module_eval so 
+      % we need to save it in a dictionary, is there another way?
+      route_identifier = verb.to_s + path
+      update_ivar 'routes, _.set(route_identifier, method)
+      % Also save the method by name so we call the method later on, HAX part 1
+      set_ivar method.name, method
+
+      % TODO instead of just matching on the path we should also incorporate
+      % splats and possible other restrictions
+
+      module_eval __FILE__, __LINE__ + 1, ~~METHOD
+        def handle_http('#{verb}, "#{path}", request)
+          route = @routes["#{route_identifier}"]
+          % Just calling route.call() doesn't seem to work since it is unbound?
+          % The following does work since we added the method as ivar, HAX part 2
+          send(route.name)
+        end
+~~
+
     end
 
     def get(route, method)
@@ -37,7 +48,7 @@ module Frankie::App
   attr_reader ['routes, 'request, 'response, 'params]
 
   def __mixed_in__(base)
-    base.set_ivar('routes, [])
+    base.set_ivar('routes, {})
     base.using Frankie::App::Routing
   end
 
--- lib/frankie/routes.ex
+++ lib/frankie/routes.ex
@@ -13,98 +13,17 @@ module Frankie::Routes
 
     def handle_http(request, response)
       verb   = request.request_method
-      path   = request.path_chars
-      {route, params} = find_route(@routes, verb, path, request)
-      result = 
-        if route.respond_to?('call, 3)
-          route.call(#(@app)(request, response, params), request, response)
-        else
-          route.call(#(@app)(request, response, params))
-        end
+      path   = request.path_chars.to_bin
 
-
+      result = @app.handle_http(verb, path, request)
+      
       if result.__module_name__ == 'String::Behavior
         result = response.body(result)
       end
-
+      
       result.dispatch!
     end
 
-    private
-
-    def find_route([h|t], verb, path, request)
-      result =
-        if h.respond_to?('match_route, 2)
-          h.match_route(verb, path) % Optimize the most common case.
-        else
-          h.match_route(request)
-        end
-
-      if result
-        {h, result}
-      else
-        find_route(t, verb, path, request)
-      end
-    end
-
-    def find_route([], _, _, _)
-      {#Frankie::Routes::ErrorRoute(404), {}}
-    end
-  end
-  
-  % Main route object that asserts for the verb,
-  % route and dispatches the stored method/callback.
-  module VerbRoute
-    attr_reader ['verb, 'path, 'method]
-
-    def __bound__(verb, path, method)
-      @('verb: verb, 'path: path.split(~r"/"), 'method: method)
-    end
-
-    def match_route(verb, path)
-      @verb == verb && match_route(@path, path.to_bin.split(~r"/"), {'splat: []})
-    end
-
-    def match_route([], [], matched)
-      matched
-    end
-
-    def match_route([<<$', key|binary>> | t1], [value|t2], matched)
-      key = key.to_atom
-      match_route(t1, t2, matched.set(key, value))
-    end
-
-    def match_route(["*" | t1], [value|t2], matched)
-      match_route(t1, t2, matched.update 'splat, _.push(value))
-    end
-      
-    def match_route([h|t1], [h|t2], matched)
-      match_route(t1, t2, matched)
-    end
-
-    def match_route(_, _, _)
-      false
-    end
-
-    def call(app)
-      @method.apply_to(app, [])
-    end
-  end
-
-  % Route that handle errors.
-  % TODO Make this invoke a hook in the app.
-  module ErrorRoute
-    def __bound__(status)
-      @('status, status)
-    end
-
-    def match_route(_)
-      {}
-    end
-
-    def call(_app, _request, response)
-      response.set(@status, {}, "Status: #{@status}")
-    end
   end
 end
 
